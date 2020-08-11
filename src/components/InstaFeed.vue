b5296a93841af8c6e234106d83ea9877d6f77aa5
Olivier Balaguer <oli@olivierbalaguer.com>
2020-08-11 12:18:21 +0100

    feed working in dev mode

--- src/components/InstaFeed.vue
+++ src/components/InstaFeed.vue
@@ -5,7 +5,7 @@
       <div v-for="edge in $static.allInstagramPhoto.edges" :key="edge.node.id" class="bg-gray-400 ">
         <a :href="edge.node.is_video ? edge.node.video_url : edge.node.display_url" target="_blank" rel="noopener noreferrer">
           <!-- <img :src="[!edge.node.is_video ? edge.node.video_url : edge.node.display_url]" alt="" class="object-contain"> -->
-          <img :src="edge.node.thumbnail_src" alt="" class="object-contain">
+          <img :src="edge.node.thumbnail_src" :alt="!edge.node.accessibility_caption == '' ? edge.node.accessibility_caption : 'Photo by @family_to_family_beirut'" class="object-contain">
         </a>
       </div>
     </div>
@@ -23,6 +23,7 @@ query {
         video_url
         thumbnail_src
         is_video
+        accessibility_caption
         edge_media_to_caption {
           edges {
             node {
