3a68a9e1c56e994e058af9166c2359b6afa7d4a7
Olivier Balaguer <oli@olivierbalaguer.com>
2020-08-11 11:26:07 +0100

    fixing gitignore caching issue

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Family to Family - Beirut Relief Fund",
-  "siteUrl": "https://family-to-family.netlify.app",
-  "version": "0.7.19",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1597067590508
